80e87250455ecc7feb0aef5c02e49f00f802c8c2
Paula <paulagc91@hotmail.com>
2022-02-22 16:57:25 +0200

    feat: add edit name functionality

--- schedule-app/src/components/Slot.tsx
+++ schedule-app/src/components/Slot.tsx
@@ -1,9 +1,15 @@
-import { Card, CardContent, IconButton, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useDrag } from "react-dnd";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { StoreContext } from "../store/storeContext";
 import ITask from "../interfaces/ITask";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const Slot = ({ id, name }: { id: number; name: string }) => {
   const { tasks, setTasks } = useContext(StoreContext);
@@ -14,22 +20,52 @@ const Slot = ({ id, name }: { id: number; name: string }) => {
       isDragging: monitor.isDragging(),
     }),
   }));
+  const [editActive, setEditActive] = useState(false);
+  const [newTitle, setNewTitle] = useState<string>(name);
 
   const handleDelete = (e: any) => {
     e.preventDefault();
     const modifiedTasks: ITask[] = [...tasks];
-    const taskIndex = modifiedTasks.findIndex(
-        (item: ITask) => id === item.id
-      );
+    const taskIndex = modifiedTasks.findIndex((item: ITask) => id === item.id);
     modifiedTasks.splice(taskIndex, 1);
     setTasks(modifiedTasks);
+  };
+
+  const handleNameSubmit = (e: any) => {
+    if (e.key === "Enter") {
+      const modifiedTasks: ITask[] = [...tasks];
+      const taskIndex: number = modifiedTasks.findIndex(
+        (item: ITask) => id === item.id
+      );
+      let taskTemp = modifiedTasks[taskIndex];
+      taskTemp.title = newTitle;
+      modifiedTasks[taskIndex] = taskTemp;
+      setTasks(modifiedTasks);
+      setNewTitle("");
+      setEditActive(false);
+    }
   };
 
   return (
     <Card ref={drag} style={{ backgroundColor: isDragging ? "grey" : "white" }}>
       <CardContent>
-        <Typography variant="subtitle1" component="div">
-          {name}
+        <Typography
+          variant="subtitle1"
+          component="div"
+          onDoubleClick={() => setEditActive(true)}
+        >
+          {editActive ? (
+            <TextField
+              style={{ maxWidth: "150px" }}
+              required
+              id="outlined-required"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              onKeyDown={(e) => handleNameSubmit(e)}
+            />
+          ) : (
+            name
+          )}
         </Typography>
         <IconButton
           aria-label="delete"
