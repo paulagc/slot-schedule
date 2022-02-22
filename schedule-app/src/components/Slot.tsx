cee9621dfaf0d007e6b22f032931f586792250d9
Paula <paulagc91@hotmail.com>
2022-02-22 16:35:35 +0200

    feat: delete task

--- schedule-app/src/components/Slot.tsx
+++ schedule-app/src/components/Slot.tsx
@@ -1,13 +1,12 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, IconButton, Typography } from "@mui/material";
 import { useDrag } from "react-dnd";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { StoreContext } from "../store/storeContext";
+import ITask from "../interfaces/ITask";
+import { useContext } from "react";
 
-const Slot = ({
-  id,
-  name,
-}: {
-  id: number;
-  name: string;
-}) => {
+const Slot = ({ id, name }: { id: number; name: string }) => {
+  const { tasks, setTasks } = useContext(StoreContext);
   const [{ isDragging }, drag] = useDrag(() => ({
     type: "task",
     item: { id: id },
@@ -16,12 +15,29 @@ const Slot = ({
     }),
   }));
 
+  const handleDelete = (e: any) => {
+    e.preventDefault();
+    const modifiedTasks: ITask[] = [...tasks];
+    const taskIndex = modifiedTasks.findIndex(
+        (item: ITask) => id === item.id
+      );
+    modifiedTasks.splice(taskIndex, 1);
+    setTasks(modifiedTasks);
+  };
+
   return (
     <Card ref={drag} style={{ backgroundColor: isDragging ? "grey" : "white" }}>
       <CardContent>
         <Typography variant="subtitle1" component="div">
           {name}
         </Typography>
+        <IconButton
+          aria-label="delete"
+          size="small"
+          onClick={(e: any) => handleDelete(e)}
+        >
+          <DeleteIcon fontSize="inherit" />
+        </IconButton>
       </CardContent>
     </Card>
   );
